94b7e88799fe1a00dcd2bc60e0924d8d0d847ed9
Marco Fugaro <marco.fugaro@gmail.com>
2022-03-29 16:32:10 +0100

    🖼  Add Canvas to example

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,21 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
 import useSpline from '../src/useSpline'
 
-function App() {
-  const result = useSpline('https://prod.spline.design/no44J0optGqOLuqn/scene.spline')
+export default function App() {
+  return (
+    <Canvas shadows dpr={[1, 1.5]} camera={{ position: [-25, 5, 125], near: 0.01 }}>
+      <Suspense fallback={null}>
+        <Scene />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+function Scene() {
+  const result = useSpline('https://prod.spline.design/Ms8MisSQZCaX4PgL/scene.spline')
 
   console.log(result)
 
   return null
 }
-
-export default App
