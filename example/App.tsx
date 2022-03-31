97127608b6e0059a44a6ea6d2132db6f59452b05
Marco Fugaro <marco.fugaro@gmail.com>
2022-03-31 12:56:13 +0100

    🍩  Added Glass Circles example

--- example/App.tsx
+++ example/App.tsx
@@ -1,21 +1,97 @@
-import { Suspense } from 'react'
+import { Suspense, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import useSpline from '../src/useSpline'
+import { OrbitControls, OrthographicCamera, Sky, useHelper } from '@react-three/drei'
+import { DirectionalLightHelper } from 'three'
 
 export default function App() {
   return (
-    <Canvas shadows dpr={[1, 1.5]} camera={{ position: [-25, 5, 125], near: 0.01 }}>
+    <Canvas shadows dpr={[1, 2]} flat>
       <Suspense fallback={null}>
+        <OrbitControls />
         <Scene />
       </Suspense>
     </Canvas>
   )
 }
 
-function Scene() {
-  const result = useSpline('https://prod.spline.design/Ms8MisSQZCaX4PgL/scene.spline')
+function Scene(props) {
+  const result = useSpline('https://prod.spline.design/JOgoI55ADZgorWoy/scene.spline')
 
   console.log(result)
 
-  return null
+  const { nodes, materials } = result
+
+  const light = useRef()
+
+  useHelper(light, DirectionalLightHelper)
+
+  return (
+    <>
+      {/* TODO added background */}
+      <color attach="background" args={['#FFE8DC']} />
+      <group {...props} dispose={null}>
+        <directionalLight
+          ref={light}
+          castShadow // TODO added castShadow
+          intensity={0.6}
+          // TODO removed decay
+          // decay={2}
+          position={[18.54, 196.55, 491.49]}
+          rotation={[-0.38, 0.04, -0.09]}
+        />
+        <OrthographicCamera
+          makeDefault
+          far={50000}
+          near={-50000}
+          position={[421.6, -431.22, 801.18]}
+          rotation={[0.45, 0.44, -0.2]}
+        />
+        {/* TODO removed personal camera */}
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Torus.geometry}
+          // TODO transmission is broken
+          // material={nodes.Torus.material}
+          material={materials['Pink']}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes['Rectangle 6'].geometry}
+          material={nodes['Rectangle 6'].material}
+          position={[-700.64, 343.77, -621.72]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes['Rectangle 5'].geometry}
+          material={nodes['Rectangle 5'].material}
+          position={[-458.87, 411.05, -435.92]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes['Rectangle 4'].geometry}
+          material={nodes['Rectangle 4'].material}
+          position={[40.66, 47, -435.92]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes['Rectangle 3'].geometry}
+          material={nodes['Rectangle 3'].material}
+          position={[-348.74, -162.23, -167.36]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes['Rectangle 2'].geometry}
+          material={nodes['Rectangle 2'].material}
+          position={[242.6, 207, -273.39]}
+        />
+      </group>
+    </>
+  )
 }
