315cd3f78e9c747af695ce45f225cb7f7bc3d6f0
Marco Fugaro <marco.fugaro@gmail.com>
2022-04-13 15:24:50 +0200

    🚨  Use any for typings

--- src/useSpline.ts
+++ src/useSpline.ts
@@ -1,7 +1,13 @@
 import SplineLoader from '@splinetool/loader'
 import { useGraph, useLoader } from '@react-three/fiber'
+import type { Object3D } from 'three'
 
-export default function useSpline(url: string) {
+type Output = {
+  nodes: Record<string, any>
+  materials: Record<string, any>
+}
+
+export default function useSpline(url: string): Output {
   const scene = useLoader(SplineLoader, url)
-  return useGraph(scene)
+  return useGraph(scene as unknown as Object3D)
 }
